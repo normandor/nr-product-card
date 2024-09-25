63f7760e78d5750f59ae2cdea2bac91743ffa877
Norman Riobo <norman.riobo@happening.xyz>
2024-09-25 15:17:07 +0200

    Version 0.0.1 lista

---
+++ test/components/ProductTitle.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductTitle, ProductCard } from '../../src/components';
+import { product1 } from '../data/products';
+
+describe('ProductTitle', () => {
+    test('debe demostrar el componente correctamente con el titulo personalizado', () => {
+        const wrapper = renderer.create(
+            <ProductTitle title="Custom Product" />
+        )
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+    test('debe demostrar el componente con el nombre del producto', () => {
+        const wrapper = renderer.create(
+            <ProductCard product={product1}>
+                {
+                    () => (
+                        <ProductTitle />
+                    )
+                }
+            </ProductCard>
+        )
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    })
+})
